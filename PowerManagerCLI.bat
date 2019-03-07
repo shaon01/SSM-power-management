a7ad94f239332c24cbe7e4e67288e14daa969ac8
Golam Shahanoor <golam.shahanoor@semcon.com>
2019-03-07 09:17:40 +0100

    updated batch file so it can be run from anywhere

--- PowerManagerCLI.bat
+++ PowerManagerCLI.bat
@@ -2,6 +2,6 @@
 IF "%1"=="" (
 echo NO ARGUMENT PROVIDED
 )ELSE (
-python3 PowerManagerCLI.py %1
+python3 C:\Tools\SSM-power-management\PowerManagerCLI.py %1
 echo in command line the argument is: %1
 )
